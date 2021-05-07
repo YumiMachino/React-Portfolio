d5b3a1336d8de859e8101b2da0accbac50023911
YumiMachino <y.y.1515m@gmail.com>
2021-05-07 16:42:18 -0700

    Update: portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,6 +2,32 @@ import ProjectCard from "./ProjectCard";
 import './Portfolio.css'
 
 const Portfolio = () => {
+
+const projects = [
+  {
+    id: 1, 
+    title: "Project1",
+    img: "../img/vancouver.jpg",
+    languages: "HTML, CSS, JavaScript",
+    projectURL: "https://www.google.com/"
+  }, 
+   {
+    id: 2, 
+    title: "Project2",
+    img: "../img/vancouver.jpg",
+    languages: "HTML, CSS, JavaScript",
+    projectURL: "https://www.google.com/"
+  }, 
+   {
+    id: 3, 
+    title: "Project3",
+    img: "../img/vancouver.jpg",
+    languages: "HTML, CSS, JavaScript",
+    projectURL: "https://www.google.com/"
+  }
+]
+
+
     return ( 
         <div className="portfolio-section">
           <div className="portfolio_title">
@@ -9,18 +35,15 @@ const Portfolio = () => {
               <p>Here, you can see projects I've worked so far!</p>
           </div>
             <div className="cards">
-                <div>
-                  <ProjectCard /> 
-                </div>
-                <div>
-                  <ProjectCard /> 
-                </div>
-                <div>
-                  <ProjectCard /> 
-                </div>
+                  {projects.map((project) => (
+                    <div className="card">
+                    <ProjectCard id= {project.id} title={project.title} img={project.img} languages={project.languages} projectURL={project.projectURL} />
+                    </div>
+                  ))}
             </div>
         </div>
      );
 }
  
 export default Portfolio;
+
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,54 +7,51 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core';
 
-const ProjectCard = () => {
 
-    // const cardStyle = {
-    //     height: 300,
-    //     backgroundColor: "#CDE1F8",
-    //     marginTop:8,
-    //     marginBottom: 19.2,
-    //     marginLeft: 20,
-    //     marginRight:20
-    // }
+const ProjectCard = (props) => {
 
+    const { title, img, languages, projectURL} = props;
+ 
     const useStyle = makeStyles({
-        root: {
-            maxWidth: 345,
+      card: {
+            maxWidth: 350,
             backgroundColor: "#CDE1F8",
             marginLeft:20,
             marginRight:20,
             marginTop:10,
-            marginBottom:10
-        },
-         }
+            marginBottom:10,
+      },
+      media: {
+        height: "300px",
+        backgroundColor:"#4287f5"
+      }
+    　}
     );
 
     const classes = useStyle();
 
 
     return ( 
-        <Card className={classes.root}> 
+        <Card className={classes.card}> 
             <CardActionArea>
-                <CardMedia
-          component="img"
-          alt="Project title"
-          height="140"
-          image="../img/vancouver.jpg"
-          title="Project title"
-        />
+                <CardMedia className={classes.media}
+                 component='img'
 
-        <CardContent  >
-          <Typography gutterBottom variant="h5" component="h2">
-            Project title
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            List of Used languages on this project
-          </Typography>
-        </CardContent>
+                 src = {img}
+                />
+         
+            <CardContent  >
+              <Typography gutterBottom variant="h5" component="h2">
+                {title}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {languages}
+              </Typography>
+            </CardContent>
+
             </CardActionArea>
-            <CardActions>
-        <Button size="small" color="primary">
+            <CardActions >
+        <Button size="small" color="primary" href={projectURL} variant='outlined'>
           Check this out
         </Button>
       </CardActions>
